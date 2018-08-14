1943dd0625fb63075728e6640239d92d70227ce2
Raghu Betina <raghu@firstdraft.com>
2018-08-14 01:10:41 +0000

    Whitelist columns in SafetyEvent for ActiveAdmin

--- app/admin/safety_events.rb
+++ app/admin/safety_events.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register SafetyEvent do
+
+ permit_params :comment, :classification_id, :user_id, :root_cause, :action_plan
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
